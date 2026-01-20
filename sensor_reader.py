11c4206e039b268eff82e92d8e869ba10f0850df
Colby Campbell <87204557+colby-campbell@users.noreply.github.com>
2026-01-20 10:10:48 -0700

    init: created the sensor_reader.py script that will be used to find common signals

---
+++ sensor_reader.py
@@ -0,0 +1,11 @@
+"""
+Colby Campbell
+(306) 750-1225
+colby.campbell@ucalgary.ca (preferred)
+"""
+
+"""
+project that reads two different sensor data files and compares them
+we will have to read from two different file formats: CSV and JSON
+then we will compare the readings from both sensors and the sensors have an accuracy of 100 meters
+"""
